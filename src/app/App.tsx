8c1bbc072b58d7b3134c1dc8a92192cdf6320ad2
igman <difariivman@gmail.com>
2026-03-07 10:59:12 +0700

    feat: add app navigator

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,39 +1,13 @@
 import React, { useEffect } from 'react';
-import { View, Text } from 'react-native';
+import AppNavigator from './navigation/AppNavigator';
 import { runMigrations } from '@/database/migrate';
-import { executeQuery } from '@/database/Database';
 
 const App = () => {
   useEffect(() => {
-    const initDB = async () => {
-      await runMigrations();
-
-      const test = async () => {
-        const result = await executeQuery(
-          "SELECT name FROM sqlite_master WHERE type='table'"
-        );
-    
-        const tables = [];
-    
-        for (let i = 0; i < result[0].rows.length; i++) {
-          tables.push(result[0].rows.item(i));
-        }
-    
-        console.log('TABLES:', tables);
-      };
-    
-      test();
-    };
-
-    initDB();
+    runMigrations();
   }, []);
-  
 
-  return (
-    <View>
-      <Text>Offline Notes App</Text>
-    </View>
-  );
+  return <AppNavigator />;
 };
 
 export default App;
---
+++ src/app/navigation/AppNavigator.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import NotesListScreen from '@/features/notes/screens/NotesListScreen';
+import NoteEditorScreen from '@/features/notes/screens/NoteEditorScreen';
+
+export type RootStackParamList = {
+  NotesList: undefined;
+  NoteEditor: undefined;
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+const AppNavigator = () => {
+    
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="NotesList"
+          component={NotesListScreen}
+          options={{ title: 'Notes' }}
+        />
+
+        <Stack.Screen
+          name="NoteEditor"
+          component={NoteEditorScreen}
+          options={{ title: 'Create Note' }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default AppNavigator;
