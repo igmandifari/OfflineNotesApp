6b5c2f9bad2037b2540f26f84970865fe942c19d
igman <difariivman@gmail.com>
2026-03-07 12:54:15 +0700

    fix: jest mocking

--- App.tsx
+++ App.tsx
@@ -4,7 +4,12 @@
  *
  * @format
  */
-import 'react-native-get-random-values';
+declare const jest: any;
+
+if (typeof jest === 'undefined') {
+  require('react-native-get-random-values');
+}
+
 import App from './src/app/App';
 
 export default App;
